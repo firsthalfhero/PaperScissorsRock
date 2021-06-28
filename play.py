b4fd526c13a16a42f07c5ec3d88a8b1833f19940
First Half Hero <george.cains@gmail.com>
2021-06-28 15:28:06 +1000

    add exit

--- play.py
+++ play.py
@@ -6,4 +6,6 @@ userPlaying = True
 while userPlaying:
     players_action = input("Please type the letter corresponding to your action (Paper[p], Scissors[s], Rock[r]:")
     scissorsPaperRock.gameEngine(players_action)
+else:
+    sys.exit()
     
